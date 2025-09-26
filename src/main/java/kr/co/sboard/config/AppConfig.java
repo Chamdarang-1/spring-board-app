b49020754868c73ece206922d53d71f789220ddf
Chamdarang-1 <joseph8034@naver.com>
2025-09-26 12:35:33 +0900

    add 0.0.2

--- src/main/java/kr/co/sboard/config/AppConfig.java
+++ src/main/java/kr/co/sboard/config/AppConfig.java
@@ -1,6 +1,6 @@
 package kr.co.sboard.config;
 
-import kr.co.sboard.intercepter.AppInfoInterceptor;
+import kr.co.sboard.intercepter.AppInfoIntercepter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,16 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
 
+    private AppInfo appInfo;
+
     @Bean
     public AppInfo getAppInfo() {
-        return new AppInfo();
+        appInfo = new AppInfo();
+        return appInfo;
     }
-
-    @Autowired
-    private AppInfo appInfo;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AppInfoInterceptor(appInfo));
+        registry.addInterceptor(new AppInfoIntercepter(appInfo));
     }
 }
